20094134663a8a7dcdf414da453e9b92c254d027
ThankfulBird <z.bera97@gmail.com>
2019-09-03 23:46:10 +0300

    repo updated

--- string_test.c
+++
@@ -1,8 +0,0 @@
-#include "ctring.h"
-
-int main(int argc, char const *argv[])
-{
-	String string = create_string("Hello World!");
-	printf("%s", convert_to_char_array(string));
-	return EXIT_SUCCESS;
-}
